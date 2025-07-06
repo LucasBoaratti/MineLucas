61815344e7f2e4f23cb84ccb451794896c1aae85
Lucas Boaratti <lucasboaratti15@gmail.com>
2025-07-06 03:25:43 -0300

    Corrigindo a palavra is_authenticated.

--- BackEnd/MineLucas_app/permissions.py
+++ BackEnd/MineLucas_app/permissions.py
@@ -8,6 +8,6 @@ class IsAdmin(BasePermission):
     
 class IsJogador(BasePermission):
     def has_permission(self, request, view):
-        if request.user.is_autheticated and request.user.funcao == "Jogador":
+        if request.user.is_authenticated and request.user.funcao == "Jogador":
             return True
         return False
