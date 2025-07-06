5d1cb35bb2165c06d2d190b2b29408e5dc961a85
Lucas Boaratti <lucasboaratti15@gmail.com>
2025-07-06 00:29:39 -0300

    Arquivo que transforma os dados das tabelas em JSON. Tabelas adicionadas: Usuário, Biomas, Criaturas, Blocos e Estruturas. Foi criado uma classe para o cadastro do usuário e uma para geração de token do usuário logado.

---
+++ BackEnd/MineLucas_app/serializers.py
@@ -0,0 +1,100 @@
+from rest_framework.serializers import ModelSerializer
+from rest_framework import serializers
+from .models import Usuario, Biomas, Criaturas, Blocos, Estruturas
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+
+class UsuarioSerializer(ModelSerializer):
+    class Meta:
+        model = Usuario
+
+        fields = "__all__"
+
+class BiomaSerializer(ModelSerializer):
+    class Meta:
+        model = Biomas
+
+        fields = "__all__"
+
+class CriaturaSerializer(ModelSerializer):
+    class Meta:
+        model = Criaturas
+
+        fields = "__all__"
+
+class BlocoSerializer(ModelSerializer):
+    class Meta:
+        model = Blocos
+
+        fields = "__all__"
+
+class EstruturaSerializer(ModelSerializer):
+    class Meta:
+        model = Estruturas
+
+        fields = "__all__"
+
+class UsuarioCadastrado(serializers.ModelSerializer): #Classe que realiza o cadastro do usuário
+    funcoes = [
+        ("Jogador", "Jogador"),
+        ("Admin", "Admin"),
+    ]
+
+    username = serializers.CharField(write_only=True)
+    email = serializers.CharField()
+    password = serializers.CharField(write_only=True)
+    confirmarSenha = serializers.CharField(write_only=True)
+    funcao = serializers.ChoiceField(choices=funcoes)
+
+    class Meta:
+        model = Usuario
+
+        fields = [
+            "username", 
+            "email",
+            "password",
+            "confirmarSenha",
+            "funcao",
+        ]
+
+        extra_kwargs = {
+            "password": {
+                "write_only": True
+            }
+        }
+     
+    def validate(self, data): #Validando as senhas
+        if data["password"] != data["confirmarSenha"]:
+            raise serializers.ValidationError("As senhas estão diferentes.")
+        return data
+    
+    def create(self, validated_data): #Criando o usuário e salvando no banco de dados
+        username = validated_data["username"]
+        email = validated_data["email"]
+        password = validated_data["password"]
+        confirmarSenha = validated_data["confirmarSenha"]
+        funcao = validated_data["funcao"]
+
+        usuario = Usuario(
+            username=username,
+            email=email,
+            password=password,
+            confirmarSenha=confirmarSenha,
+            funcao=funcao,
+        )   
+
+        usuario.set_password(validated_data["password"])
+
+        usuario.save()
+
+        return usuario
+    
+class LoginUsuario(TokenObtainPairSerializer):
+    def validate(self, attrs):
+        data = super().validate(attrs)
+
+        data["Usuário"] = {
+            "Nome": self.user.username,
+            "Função": self.user.funcao,
+        }
+
+        return data
