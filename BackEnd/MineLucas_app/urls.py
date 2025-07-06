e616428b5517ae9afbb8efefe4bf3a3dcb9075e8
Lucas Boaratti <lucasboaratti15@gmail.com>
2025-07-06 03:26:18 -0300

    Adicionando uma barra faltante no final de biomas.

--- BackEnd/MineLucas_app/urls.py
+++ BackEnd/MineLucas_app/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path('cadastro/', view=CadastroUsuario.as_view(), name="Cadastro do usuário"),
     path('login/', view=UsuarioLogado.as_view(), name="Login do usuário"),
     path('usuarios/', view=VerUsuarios.as_view(), name="Ver usuários cadastrados."),
-    path('biomas', view=LCBiomas.as_view(), name="Ver e criar biomas."),
+    path('biomas/', view=LCBiomas.as_view(), name="Ver e criar biomas."),
     path('biomas/<int:pk>/', view=UDBiomas.as_view(), name="Atualizar e deletar biomas."),
     path('criaturas/', view=LCCriaturas.as_view(), name="Ver e criar criaturas."),
     path('criaturas/<int:pk>/', view=UDCriaturas.as_view(), name="Atualizar e deletar criaturas."),
