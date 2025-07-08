47fb8d9f6409efe13995855d699258624f0951c0
Lucas Boaratti <lucasboaratti15@gmail.com>
2025-07-07 23:19:13 -0300

    Novas URLS de jogadores de exportações adicionadas.

--- BackEnd/MineLucas_app/urls.py
+++ BackEnd/MineLucas_app/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import UsuarioLogado, VerUsuarios, LCBiomas, UDBiomas, LCCriaturas, UDCriaturas, LCBlocos, UDBlocos, LCEstruturas, UDEstrutras, CadastroUsuario
+from .views import UsuarioLogado, VerUsuarios, LCBiomas, UDBiomas, LCCriaturas, UDCriaturas, LCBlocos, UDBlocos, LCEstruturas, UDEstrutras, LCJogadores, UDJogadores, CadastroUsuario, GeradorSeeds, ExportarBiomasJSON, ExportarCriaturasJSON, ExportarBlocosJSON, ExportarEstruturasJSON, ExportarJogadoresJSON
 
 urlpatterns = [
     path('cadastro/', view=CadastroUsuario.as_view(), name="Cadastro do usuário"),
@@ -13,4 +13,12 @@ urlpatterns = [
     path('blocos/<int:pk>/', view=UDBlocos.as_view(), name="Atualizar e deletar blocos."),
     path('estruturas/', view=LCEstruturas.as_view(), name="Ver e criar estruturas."),
     path('estruturas/<int:pk>/', view=UDEstrutras.as_view(), name="Atualizar e deletar estruturas."),
+    path('jogadores/', view=LCJogadores.as_view(), name="Ver e criar jogadores/youtubers."),
+    path('jogadores/<int:pk>/', view=UDJogadores.as_view(), name="Atualizar e deletar jogadores."),
+    path('seed/', view=GeradorSeeds.as_view(), name="Gerador de seeds no Minecraft."),
+    path('exportarBiomas/', view=ExportarBiomasJSON.as_view(), name="Exportar biomas em JSON."),
+    path('exportarCriaturas/', view=ExportarCriaturasJSON.as_view(), name="Exportar criaturas em JSON."),
+    path('exportarBlocos/', view=ExportarBlocosJSON.as_view(), name="Exportar blocos em JSON."),
+    path('exportarEstruturas/', view=ExportarEstruturasJSON.as_view(), name="Exportar estruturas em JSON."),
+    path('exportarJogadores/', view=ExportarJogadoresJSON.as_view(), name="Exportar jogadores em JSON."),
 ]
