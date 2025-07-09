bb474ab5d1bbdae49f800fb7eff4122a763d7dd5
Lucas Boaratti <lucasboaratti15@gmail.com>
2025-07-09 02:55:17 -0300

    Modal de cadastro de sucesso finalizado.

---
+++ FrontEnd/src/Components/CadastroModalSucesso.jsx
@@ -0,0 +1,25 @@
+import css from "../Styles/CadastroModalSucesso.module.css";
+import { useNavigate } from "react-router-dom";
+
+export function CadastroModalSucesso({ openModal }) {
+    if(!openModal) {
+        return null;
+    }
+
+    const navigate = useNavigate();
+
+    return (
+        <main className={css.modalContainer}>
+            <section className={css.modalCadastro}>
+                <h1>Cadastro realizado com sucesso! Realize o login.</h1>
+                    <div className={css.botao}>
+                        <button 
+                            type="button"
+                            onClick={() => navigate("/")}>
+                            Avançar
+                        </button>
+                    </div>
+            </section>
+        </main>
+    );
+}
