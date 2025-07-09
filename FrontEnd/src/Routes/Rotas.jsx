905612011558f21c1d0080c7df9b900c8af7da5b
Lucas Boaratti <lucasboaratti15@gmail.com>
2025-07-09 03:00:34 -0300

    Rotas do cadastro e da home adicionadas.

--- FrontEnd/src/Routes/Rotas.jsx
+++ FrontEnd/src/Routes/Rotas.jsx
@@ -1,12 +1,22 @@
 import { Route, Routes } from "react-router-dom";
 import { Index } from "../Pages/Index";
 import { Login } from "../Pages/Login";
+import { Cadastro } from "../Pages/Cadastro";
+import { Home } from "../Pages/Home";
 
 export function Rotas() {
     return (
         <Routes>
             <Route path="/">
                 <Route index element={<Login/>}/>
+            </Route>
+
+            <Route path="/cadastro">
+                <Route index element={<Cadastro/>}/>
+            </Route>
+
+            <Route path="/home" element={<Index/>}>
+                <Route index element={<Home/>}/>
             </Route>
         </Routes>
     );
