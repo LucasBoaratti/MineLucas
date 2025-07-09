c7aa3b54654df86f096b339adebd522c33f88964
Lucas Boaratti <lucasboaratti15@gmail.com>
2025-07-09 02:56:50 -0300

    Rodapé da página criado e finalizado.

---
+++ FrontEnd/src/Components/Rodape.jsx
@@ -0,0 +1,9 @@
+import css from "../Styles/Rodape.module.css";
+
+export function Rodape() {
+    return (
+        <footer>
+            <h1 className={css.direitosAutorais}>MineLucas ©2025, todos os direitos reservados.</h1>
+        </footer>
+    );
+}
