248af9553be9250d9073eb7c7fa1eed1493b883f
Lucas Boaratti <lucasboaratti15@gmail.com>
2025-07-09 02:59:20 -0300

    Index alterado, foi adicionado o rodapé.

--- FrontEnd/src/Pages/Index.jsx
+++ FrontEnd/src/Pages/Index.jsx
@@ -1,10 +1,18 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import { Cabecalho } from "../Components/Cabecalho";
+import { Rodape } from "../Components/Rodape";
 
 export function Index() {
-    <>
-        <Cabecalho/>
-        <Outlet/>
-    </>
+    return (
+        <>
+            <div style={{ display:"flex", flexDirection:"column", minHeight:"100vh" }}>
+                <Cabecalho/>
+                <div style={{ flex:"1" }}>
+                    <Outlet/>
+                </div>
+                <Rodape/>
+            </div>
+        </>
+    );
 }
