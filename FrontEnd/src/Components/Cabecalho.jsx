0d3e97fbb449cd3c50a115c129a07b6092040fcd
Lucas Boaratti <lucasboaratti15@gmail.com>
2025-07-09 02:54:37 -0300

    Cabeçalho atualizado.

--- FrontEnd/src/Components/Cabecalho.jsx
+++ FrontEnd/src/Components/Cabecalho.jsx
@@ -2,18 +2,25 @@ import Logo from "../assets/Images/Logo_MineLucas.png";
 import css from "../Styles/Cabecalho.module.css";
 
 export function Cabecalho() {
+    const nomeUsuario = localStorage.getItem("nomeUsuario");
+    const funcaoUsuario = localStorage.getItem("funcaoUsuario");
+
     return (
         <header>
-            <img src={Logo} alt="Logo da MineLucas com fundo verde, uma carinha de Creeper preta e o nome do site, MineLucas em letras brancas." />
-            <div className={css.links}>
-                <p>Biomas</p>
-                <p>Criaturas</p>
-                <p>Blocos</p>
-                <p>Estruturas</p>
-                <p>Jogadores</p>
-                <p>Seeds</p>
-                <p>Mapas</p>
-            </div>
+            <section className={css.cabecalho}> 
+                <img src={Logo} alt="Logo da MineLucas com fundo verde, uma carinha de Creeper preta e o nome do site, MineLucas em letras brancas." />
+                <div className={css.links}>
+                    <p>Seeds</p>
+                    <p>Mapas</p>  
+                    <p>Tutoriais</p>
+                    <p>Mods</p>  
+                </div>
+                <div className={css.perfil}>
+                    <h2>Perfil ativo:</h2>
+                    <p>{nomeUsuario}</p>
+                    <p>{funcaoUsuario}</p>
+                </div>
+            </section>
         </header>
     );
 }
