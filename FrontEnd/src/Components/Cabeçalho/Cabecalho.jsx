6fc2c5a70371db32c3d5a0810e8dfa71c7371c82
Lucas Boaratti <lucasboaratti15@gmail.com>
2025-07-11 02:05:30 -0300

    Adicionado o link da página home, além de sua estilização.

--- FrontEnd/src/Components/Cabeçalho/Cabecalho.jsx
+++ FrontEnd/src/Components/Cabeçalho/Cabecalho.jsx
@@ -1,15 +1,19 @@
-import Logo from "../assets/Images/Logo_MineLucas.png";
+import Logo from "../../assets/Images/Logo_MineLucas.png";
 import css from "./Cabecalho.module.css";
+import { useNavigate } from "react-router-dom";
 
 export function Cabecalho() {
     const nomeUsuario = localStorage.getItem("nomeUsuario");
     const funcaoUsuario = localStorage.getItem("funcaoUsuario");
+
+    const navigate = useNavigate();
 
     return (
         <header>
             <section className={css.cabecalho}> 
                 <img src={Logo} alt="Logo da MineLucas com fundo verde, uma carinha de Creeper preta e o nome do site, MineLucas em letras brancas." />
                 <div className={css.links}>
+                    <p onClick={() => navigate("/home")} className={css.home}>Home</p>
                     <p>Seeds</p>
                     <p>Mapas</p>  
                     <p>Tutoriais</p>
